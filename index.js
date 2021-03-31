4a504a196dff9398bb204ebbd0a021fb7e29d5ad
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-31 17:39:20 +0300

    localStorage

--- index.js
+++ index.js
@@ -14,16 +14,18 @@ function func(){
     }
 }
 
+function uuid4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
 
 function addToDoItemWithRandomColor(){
     const color = document.querySelector(`.c${Math.floor(Math.random() * Math.floor(6))}`);
     console.log(color);
-    const id = new Date();
-    // const toDoInner = document.createElement('div')
-    // toDoInner.className = 'toDoInner';
-    // // toDoInner.id = `${id}`;
-    // inner.append(toDoInner);
+    const id = uuid4();
 
     const toDoItem = document.createElement('li')
     toDoItem.className = 'toDoItem';
@@ -52,16 +54,11 @@ function addToDoItemWithRandomColor(){
 
 function addToDoItem(){
     const color = document.querySelector('.active');
-    const id = new Date();
-    // const toDoInner = document.createElement('div')
-    // toDoInner.className = 'toDoInner';
-    // // toDoInner.id = `${id}`;
-    // // toDoInner.style.backgroundColor = `${color.value}`
-    // inner.append(toDoInner);
+    const id = uuid4();
 
     const toDoItem = document.createElement('li')
     toDoItem.className = 'toDoItem';
-    toDoItem.innerText = input.value;
+    toDoItem.value = input.value;
     toDoItem.id = `${id}`;
     toDoItem.style.backgroundColor = `${color.value}`;
     inner.append(toDoItem);
@@ -69,22 +66,46 @@ function addToDoItem(){
     const toDoCheckbox = document.createElement('input');
     toDoCheckbox.className = 'toDoCheckbox';
     toDoCheckbox.type = 'checkbox';
-    // toDoCheckbox.style.backgroundColor = `${color.value}`;
     toDoCheckbox.addEventListener('change', checkedBox);
     toDoItem.prepend(toDoCheckbox);
     console.log(color.value);
 
     const dltButton = document.createElement('button');
     dltButton.type = 'button';
+    dltButton.className = 'dltButton';
     dltButton.innerText = 'del';
     dltButton.addEventListener('click', function (){
         document.getElementById(`${id}`).remove();
     });
     toDoItem.append(dltButton)
+
+    function parseElem(){
+        let a = {};
+        a.tagName = toDoItem.tagName;
+        a.className = toDoItem.getAttribute('class');
+        a.innerText = toDoItem.getAttribute('innerText');
+        a.id = toDoItem.getAttribute('id');
+        a.style = toDoItem.getAttribute('style')
+        localStorage.setItem(`1`, JSON.stringify(a))
+
+    }
+    parseElem();
+}
+
+function loadItem(){
+    let a;
+    a = JSON.parse(localStorage.getItem('1'));
+    const toDoItem = document.createElement(a.tagName)
+    toDoItem.className = a.className;
+    toDoItem.value = a.innerText;
+    toDoItem.id = a.id;
+    toDoItem.style = a.style;
+    inner.append(toDoItem);
+    console.log(toDoItem);
+    console.log('shift');
 }
 
 function checkedBox(){
-    // const parent =
     console.log(this.parentNode)
     let div = document.getElementById(`${this.parentNode.id}`);
     let checkBox = document.querySelectorAll('.toDoCheckbox');
@@ -102,12 +123,17 @@ function checkedBox(){
 document.querySelector('.addToDo').addEventListener('click', function(){
     const color = document.querySelector('.active');
     if(color === null){
-        addToDoItemWithRandomColor()
+        addToDoItemWithRandomColor();
     }else {
         addToDoItem();
         document.querySelector('.active').checked = false;
         document.querySelector('.active').classList.toggle('active', false);
     }
+    // addToStorage()
 });
 
-
+// document.querySelector('.addToDo').addEventListener('click', addToStorage);
+//
+function clearLocal(){
+    localStorage.clear();
+}
